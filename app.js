e99d2dfd536dc235bbc374962add629341ddd855
Mmadhwam <138669732+Mmadhwam@users.noreply.github.com>
2023-08-01 08:13:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require ('express');
 const app = express()
 var port = process.env.PORT || 4000;
-app.get('/', (req,res) => res.send('Hello World !!'));
+app.get('/', (req,res) => res.send('Welcome to Devops !!'));
 app.listen (port, () => console.log('server is running on ' + port));
